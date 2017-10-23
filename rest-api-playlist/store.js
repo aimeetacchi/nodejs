d1cef4072930e0e902eb94b473894339c9ca882f
darkxangel84 <aimeetacchi@hotmail.co.uk>
2017-10-23 12:23:29 +0100

    Encrypt pws tut

--- rest-api-playlist/store.js
+++ rest-api-playlist/store.js
@@ -1,11 +1,36 @@
-const knex = require('knex')(require('./knexfile'))
+const crypto = require('crypto');
+const knex = require('knex')(require('./knexfile'));
 
 module.exports = {
+	saltHashPassword,
 	createUser ({ username, password }) {
-		console.log(`Add user ${username} with password ${password}`);
+		console.log(`Add user ${username}`);
+
+		// call saltHashPassword function ---
+		const { salt, hash } = saltHashPassword(password);
+		// return the data inputted into the form and store and in DB
 		return knex('user').insert({
-			username,
-			password
+			salt,
+			encrypted_password: hash,
+			username
 		})
 	}
+}
+
+// saltHashPassword Function -
+function saltHashPassword (password) {
+	// call randomString function -
+	const salt = randomString()
+	const hash = crypto
+	.createHmac('sha512', salt)
+	.update(password)
+	return {
+		salt,
+		hash: hash.digest('hex')
+	}
+}
+
+// randomString Function -
+function randomString () {
+	return crypto.randomBytes(4).toString('hex');
 }
